0bcec7acd307305174e155ab4cd562f4a32de1a9
BartoszF <felis.bartosz@gmail.com>
2019-07-12 15:02:30 +0200

    Init

---
+++ src/main/java/pl/axit/ppleague/PpleagueApplication.java
@@ -0,0 +1,13 @@
+package pl.axit.ppleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpleagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpleagueApplication.class, args);
+    }
+
+}
