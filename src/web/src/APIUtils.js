d6f389b4880ddde7232a5191bfa8c9ff0081aca3
BartoszF <felis.bartosz@gmail.com>
2019-11-05 15:48:51 +0100

    Api

--- src/web/src/APIUtils.js
+++ src/web/src/APIUtils.js
@@ -1,10 +1,12 @@
-
-import { ACCESS_TOKEN } from './constants';
+import {ACCESS_TOKEN} from './constants';
 
 export function request(options) {
     const headers = new Headers({
         'Content-Type': 'application/json',
     })
+
+    //TODO: Dev?
+    const API_URL = "https://ppleague-backend.herokuapp.com/";
 
     if (localStorage.getItem(ACCESS_TOKEN)) {
         headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
@@ -13,7 +15,7 @@ export function request(options) {
     const defaults = {headers: headers};
     options = Object.assign({}, defaults, options);
 
-    return fetch(options.url, options)
+    return fetch(API_URL + options.url, options)
         .then(response =>
             response.json().then(json => {
                 if (!response.ok) {
