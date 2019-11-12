3f9fd7beb9149c90ec3450cb5093b80d9904da84
BartoszF <felis.bartosz@gmail.com>
2019-11-12 10:18:55 +0100

    Highlight ongoing match

--- src/web/src/components/playerList/playerListItem.jsx
+++ src/web/src/components/playerList/playerListItem.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Icon } from 'antd';
-import {observer, inject} from "mobx-react";
+import {Icon} from 'antd';
+import {inject, observer} from "mobx-react";
 import "./PlayerListItem.css";
 
 import _ from 'lodash';
@@ -14,11 +14,21 @@ class PlayerListItem extends React.Component {
         return !_.isEmpty(this.props.matchStore.ongoingMatch) && this.props.userStore.player.playerId != playerId &&(this.props.matchStore.ongoingMatch.playerA.id == playerId || this.props.matchStore.ongoingMatch.playerB.id == playerId)
     }
 
+    getIcons() {
+        let icons = []
+        if (this.isOngoing(this.props.player.playerId))
+            icons.push(<Icon height={'30px'} type="hourglass"/>);
+        //<Icon type="clock-circle" />
+
+        return icons;
+    }
+
     render() {
         return (
-            <div key={this.props.player.playerId} onClick={(ev) => this.props.click(ev,this.props.player)} className={"playerListItem"}>
+            <div key={this.props.player.playerId} onClick={(ev) => this.props.click(ev, this.props.player)}
+                 className={"playerListItem " + (this.isOngoing(this.props.player.playerId) ? "ongoing" : "")}>
                 <span className={"rank"}><h2>{this.props.player.standing} </h2></span>
-                <span className={"icons"}>{this.isOngoing(this.props.player.playerId) ? <Icon height={'30px'} type="hourglass" /> : ""}</span>
+                <span className={"icons"}>{this.getIcons()}</span>
                 <span className={"name"}><h2>{this.props.player.name}</h2></span>
             </div>)
     }
