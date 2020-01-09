b48bf8757dbcf237e1282f94213b27b6cf0acbbc
BartoszF <felis.bartosz@gmail.com>
2020-01-09 15:24:11 +0100

    Searchbar

--- src/web/src/components/playerList/playerList.jsx
+++ src/web/src/components/playerList/playerList.jsx
@@ -3,9 +3,19 @@ import { inject, observer } from 'mobx-react';
 import PlayerListItem from './playerListItem';
 import './playerList.css';
 import MatchService from '../../services/MatchService';
+import { Input } from 'antd';
 
 @inject('matchStore') @inject('playerStore') @observer
 class PlayerList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: '',
+    };
+  }
+
   onClick(ev, player) {
     if (!this.props.public && player.playerId === this.props.playerStore.userPlayer.playerId) {
       return;
@@ -22,15 +32,40 @@ class PlayerList extends React.Component {
       });
   }
 
+  onSearchChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
+  filterPlayers(value) {
+    const { search } = this.state;
+    if (search.length >= 3) {
+      if (value.name.toLowerCase()
+        .includes(search.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   render() {
+    const { players } = this.props;
     return (
-      <div className="playerList">
-        {this.props.players.map((player, index) => (
-          <PlayerListItem key={player.playerId}
-            public={this.props.public}
-            click={this.onClick.bind(this)}
-            player={player} />
-        ))}
+      <div className="playerListContainer">
+        <Input onChange={this.onSearchChange} placeholder='Username'/>
+        <div className="playerList">
+          {players.filter(this.filterPlayers.bind(this))
+            .map((player, index) => (
+              <PlayerListItem
+                key={player.playerId}
+                public={this.props.public}
+                click={this.onClick.bind(this)}
+                player={player}
+              />
+            ))}
+        </div>
       </div>
     );
   }
